f15e323fbca093d1088c7544b4a0a727c8d20eb9
WizardOfYendor1 <WizardOfYendor1@users.noreply.github.com>
2026-09-13 17:32:13 -0400

    - Slow down the marquee auto scroll just a wee bit.

--- lib/ui/screens/livetv/epg/widgets/epg_hero_preview.dart
+++ lib/ui/screens/livetv/epg/widgets/epg_hero_preview.dart
@@ -133,7 +133,7 @@ class EpgHeroPreview extends StatelessWidget {
             text: synopsis!,
             maxLines: 3,
             style: synopsisStyle,
-            millisPerPixel: 35,
+            millisPerPixel: 40,
             pauseDurationMs: 1600,
             showDotSeparator: false,
           ),
